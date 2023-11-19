0b8ec00bcd1450b401a64c14e168258f3493f46b
jtng189 <jtan189@uw.edu>
2023-11-18 17:07:57 -0800

    added search query with name & address filtering

--- src/components/locator/PharmacyList.js
+++ src/components/locator/PharmacyList.js
@@ -7,30 +7,64 @@ import { PharmacyCard } from "./PharmacyCard";
 // Take user input of "search", "name" OR "address" filters
 // Show only matching pharmacies in the list
 export function PharmacyList(props) {
-    
     const pharmacyArray = props.list;
+    const [search, setSearch] = useState("");
+    const [filter, setFilter] = useState(null);
+
+    const searchFilter = (event) => {
+        setSearch(event.target.value);
+    }
+
+    const onChangeFilter = (event) => {
+        setFilter(event.target.value);
+    }
 
     const pharmacyCardArray = pharmacyArray.map((pharmacy) => {
-        return (
-            <PharmacyCard
-                pharmacy={pharmacy}
-                selectPharmacy={props.selectPharmacy}
-                key={pharmacy.key}
-                checked={props.selectedPharmacy.name === pharmacy.name}
-            />
-        );
+        // If name filter is selected, return pharmacies whose name contains the search
+        // Else if address filter is selected, return pharmacies whose name contains the search
+        // Else, return pharmacies whose name or address contains the search
+        if (filter === "name" && pharmacy.name.includes(search)) {
+            return (
+                <PharmacyCard
+                    pharmacy={pharmacy}
+                    selectPharmacy={props.selectPharmacy}
+                    key={pharmacy.key}
+                    checked={props.selectedPharmacy.name === pharmacy.name}
+                />
+            );
+        } else if (filter === "address" && pharmacy.address.includes(search)) {
+            return (
+                <PharmacyCard
+                    pharmacy={pharmacy}
+                    selectPharmacy={props.selectPharmacy}
+                    key={pharmacy.key}
+                    checked={props.selectedPharmacy.name === pharmacy.name}
+                />
+            );
+        } else {
+            if (!filter && (pharmacy.name.includes(search) || pharmacy.address.includes(search))) {
+                return (
+                    <PharmacyCard
+                        pharmacy={pharmacy}
+                        selectPharmacy={props.selectPharmacy}
+                        key={pharmacy.key}
+                        checked={props.selectedPharmacy.name === pharmacy.name}
+                    />
+                );
+            }
+        }
     });
 
     return (
         <div className="pharmacy-list col col-sm col-md col-lg">
             <section className="locator">
                 <h3><i className="material-icons" aria-label="Pharmacy List"><ListIcon /></i>Pharmacy List</h3>
-                <input type="radio" id="name-filter" name="filter"></input>
+                <input type="radio" id="name-filter" name="filter" value="name" onChange={onChangeFilter} checked={filter === "name"}></input>
                 <label className="filter-spacing" htmlFor="name-filter">Name</label>
-                <input type="radio" id="address-filter" name="filter"></input>
-                <label className="filter-spacing" htmlFor="address-filter">Address</label><br></br>
+                <input type="radio" id="address-filter" name="filter" value="address" onChange={onChangeFilter} checked={filter === "address"}></input>
+                <label className="filter-spacing" htmlFor="address-filter">Address</label><br />
                 <i className="material-icons"><SearchIcon /></i>
-                <input id="search-pharmacy" type="text" placeholder="Search.."></input>
+                <input id="search-pharmacy" type="text" placeholder="Search.." onChange={searchFilter}></input>
                 {/* Replace below card with component */}
                 {/* This should render an array of all the corresponding pharmacies */}
                 {pharmacyCardArray}
