ebecc71297107aa27b1fe152275b26a3636078c6
jtng189 <jtan189@uw.edu>
2023-12-04 12:12:13 -0800

    fixed indentation

--- src/components/locator/PharmacyInformation.js
+++ src/components/locator/PharmacyInformation.js
@@ -42,32 +42,31 @@ export function PharmacyInformation(props) {
         if (pharmacy.name === "") {
             return (
                 // <div className="card-body">
-                    <p className="center-text">Select a pharmacy from the list to display its information.</p>
+                <p className="center-text">Select a pharmacy from the list to display its information.</p>
                 // </div>
             );
         } else {
             return (
-
-            <div className="card-body">
-                <div>
-                    <a style={{ textDecoration: 'none' }}
-                        href={"https://maps.google.com/?q=" + address}>
-                        <i className="material-icons" aria-label="Pharmacy location"><HomeIcon /></i>
-                    </a>
-                    <span>Location:</span> <br />
-                    <input type="text" id="location" name="location" onChange={addressChange} value={address}
-                    ></input>
+                <div className="card-body">
+                    <div>
+                        <a style={{ textDecoration: 'none' }}
+                            href={"https://maps.google.com/?q=" + address}>
+                            <i className="material-icons" aria-label="Pharmacy location"><HomeIcon /></i>
+                        </a>
+                        <span>Location:</span> <br />
+                        <input type="text" id="location" name="location" onChange={addressChange} value={address}
+                        ></input>
+                    </div>
+                    <div>
+                        <a style={{ textDecoration: 'none' }} href={"tel:" + phoneNumber}>
+                            <i className="material-icons" aria-label="Call Pharmacy Phone Number"><CallIcon /></i>
+                        </a>
+                        <span >Number (#):</span> <br />
+                        <input type="text" id="phoneNum" name="phoneNum" onChange={phoneNumberChange} value={phoneNumber}
+                        ></input>
+                    </div>
+                    <PharmacyNotes pharmacy={pharmacy} newPharmacy={newPharmacy} edit={props.edit} delete={deleteInformation} list={props.list} />
                 </div>
-                <div>
-                    <a style={{ textDecoration: 'none' }} href={"tel:" + phoneNumber}>
-                        <i className="material-icons" aria-label="Call Pharmacy Phone Number"><CallIcon /></i>
-                    </a>
-                    <span >Number (#):</span> <br />
-                    <input type="text" id="phoneNum" name="phoneNum" onChange={phoneNumberChange} value={phoneNumber}
-                    ></input>
-                </div>
-                <PharmacyNotes pharmacy={pharmacy} newPharmacy={newPharmacy} edit={props.edit} delete={deleteInformation} list={props.list} />
-            </div>
             );
         }
     }
