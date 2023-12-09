d3ca3680b7f32240caec715de29d6067127f2f63
jtng189 <jtan189@uw.edu>
2023-12-09 15:09:11 -0800

    added catch() and error messages for invalid API KEY/address

--- src/components/locator/PharmacyMap.js
+++ src/components/locator/PharmacyMap.js
@@ -13,45 +13,64 @@ const API_KEY = '';
 export function PharmacyMap(props) {
     const [name, setName] = useState("");
     const [location, setLocation] = useState({ lat: 47.65485149091028, lng: -122.30749748366422 });
+    const [showError, setShowError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    
     useEffect(() => {
         const loader = new Loader({
             apiKey: API_KEY,
             version: "weekly",
             libraries: ["geocoding"]
-        })
-        loader.importLibrary('geocoding').then(() => {
-            const geocoder = new window.google.maps.Geocoder();
-
-            geocoder.geocode({ address: props.pharmacy.address }, (results, status) => {
-                if (status === 'OK') {
-                    setLocation({
-                        lat: results[0].geometry.location.lat(),
-                        lng: results[0].geometry.location.lng(),
-                    });
-                } else {
-                    console.log("Geocoding did not return any results.");
-                }
-            }
-            );
         });
+
+        loader.importLibrary('geocoding')
+            .then(() => {
+                const geocoder = new window.google.maps.Geocoder();
+
+                geocoder.geocode({ address: props.pharmacy.address }, (results, status) => {
+                    if (status === 'OK') {
+                        setLocation({
+                            lat: results[0].geometry.location.lat(),
+                            lng: results[0].geometry.location.lng(),
+                        });
+                        setShowError(false);
+                    } else {
+                        setErrorMessage("Geocoding did not return any results/API Key is no longer available for use.");
+                        setShowError(true);
+                    }
+                });
+            })
+            .catch(function(err) {
+                setErrorMessage(err);
+                setShowError(true);
+            });
     }, [props.pharmacy.address]);
 
     useEffect(() => {
         setName(props.pharmacy.name || "");
     }, [props.pharmacy]);
 
+    function DisplayMap() {
+        if (showError) {
+            return <p>{errorMessage}</p>;
+        } else {
+            return (
+            <APIProvider apiKey={API_KEY}>
+                <Map zoom={15} center={location} >
+                    <Marker position={location} />
+                </Map>
+            </APIProvider>
+            );
+        }
+    }
+
+    console.log(showError);
     return (
         <div className="col col-sm col-md col-lg-5">
             <section className="locator">
                 <h3>Map Information: <span className="underline">{name}</span></h3>
                 <div className="card map mx-auto">
-                    <APIProvider apiKey={API_KEY}>
-                        <Map zoom={15} center={location} >
-                            <Marker position={location} />
-                        </Map>
-                    </APIProvider>
+                    {DisplayMap()}
                 </div>
             </section>
         </div>
